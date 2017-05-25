9de24142d29c4bf13707cfe68d5285a9bda64177
Jimmy Shih <jimmy.shih@hp.com>
2017-05-25 11:46:06 -0700

    changed pyramid functionality from user input to arguments

--- pyramid.c
+++ pyramid.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
-int main()
+int main(int argc, char *argv[])
 {
     int i, j, rows;
+    if (argc > 1){
+        fprintf(stderr, "Invalid Arguments\n", );
+    }
+    else{
+        rows = argv[1]; 
 
-    printf("Enter number of rows: ");
-    scanf("%d",&rows);
+    //printf("Enter number of rows: ");
+    //scanf("%d",&rows);
 
-    for(i=1; i<=rows; ++i)
-    {
-        for(j=1; j<=i; ++j)
+        for(i=1; i<=rows; ++i)
         {
-            printf("* ");
+            for(j=1; j<=i; ++j)
+            {
+                printf("* ");
+            }
+            printf("\n");
         }
-        printf("\n");
     }
     return 0;
 }
