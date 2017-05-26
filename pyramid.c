fc691bdc4c553cd8b47bf6153387998349671799
Jimmy Shih <jimmy.shih@hp.com>
2017-05-26 10:53:48 -0700

    added comments

--- pyramid.c
+++ pyramid.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
 
     //printf("Enter number of rows: ");
     //scanf("%d",&rows);
-
+	// loop to print stars
         for(i=1; i<=rows; ++i)
         {
             for(j=1; j<=i; ++j)
